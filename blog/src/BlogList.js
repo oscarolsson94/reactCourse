1cb3c8fc44cc61b7260f0bd37625a3a4dd40f834
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-02-24 14:35:07 +0100

    using function as props

--- blog/src/BlogList.js
+++ blog/src/BlogList.js
@@ -1,4 +1,4 @@
-const BlogList = ({ blogs, title}) => {
+const BlogList = ({ blogs, title, handleDelete }) => {
   // const blogs = props.blogs;
   // const title = props.title;
   // console.log(blogs);
@@ -9,7 +9,8 @@ const BlogList = ({ blogs, title}) => {
       {blogs.map(blog => (
         <div className="blog-preview" key={blog.id} >
           <h2>{ blog.title }</h2>
-          <p>Written by { blog.author }</p>
+              <p>Written by {blog.author}</p>
+              <button onClick={() => handleDelete(blog.id)}>delete blog</button>
         </div>
       ))}
     </div>
