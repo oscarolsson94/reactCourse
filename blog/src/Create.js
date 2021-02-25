f8adb55ff6e5a9497d1b46e4d8ce84bb9534085c
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-02-25 12:47:30 +0100

    submit events

--- blog/src/Create.js
+++ blog/src/Create.js
@@ -5,10 +5,15 @@ const Create = () => {
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
 
+    const handleSubmit = (e) => {
+        e.preventDefault(); // prevents page from refreshing on submit
+        const blog = {title, body, author}
+    }
+
     return ( 
         <div className="create">
             <h2>Add a New Blog</h2>
-            <form>
+            <form onSubmit={}>
                 <label>Blog title:</label>
                 <input
                     type="text"
