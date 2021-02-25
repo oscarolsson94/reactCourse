84a319a27ce0bed0d55a700e927d69e7be39236f
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-02-25 11:38:38 +0100

    useEffect Cleanup

--- blog/src/useFetch.js
+++ blog/src/useFetch.js
@@ -6,8 +6,10 @@ const useFetch = (url) => { //custom hooks have to start with "use"
     const [isPending, setIsPending] = useState(true);
     const [error, setError] = useState(null);
 
-     useEffect(() => {
-        fetch(url) //fetch data from our json-server
+    useEffect(() => {
+        const abortCont = new AbortController(); 
+
+        fetch(url, { signal: abortCont.signal }) //fetch data from our json-server
             .then(res => {
                 if (!res.ok) { // error coming back from server
                     throw Error('could not fetch the data for that resource');
@@ -18,12 +20,19 @@ const useFetch = (url) => { //custom hooks have to start with "use"
                 console.log(data); //data is now an array of objects
                 setData(data);
                 setIsPending(false); //used to create loading message while data is being fetched
-            })
-            .catch(err => {
-                setIsPending(false);
-                setError(err.message);
                 setError(null);
             })
+            .catch(err => {
+                if (err.name === 'AbortError') {
+                    console.log('fetch aborted');
+                }
+                else{
+                setIsPending(false);
+                    setError(err.message);    
+                }    
+            })
+         
+        return () => abortCont.abort();
      }, [url]);
     
     return {data, isPending, error}
